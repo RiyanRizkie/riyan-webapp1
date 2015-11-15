30664e49357881808817f03905af11aa087c1fd8
Riyan Alfa Rizkie <Riyan Alfa Rizkie>
2015-11-15 11:19:33 +0700

    tambah file hello.php

---
+++ hello.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>web app sederhana</title>
+</head>
+<body>
+<?php
+if(!empty($_POST)){
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
+
+	echo '<h1>Halo' . $nama . '!</h1>';
+	echo 'Email : ' . $email;
+}
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo"Hello Azure";
-?>
+<html>
+<head><title>web app sederhana</title>
+</head>
+<body>	
+<form action="hello.php" method="POST">
+    nama : <input type="text" name="nama"/>
+    <br>
+    email : <input type="email" name="email"/>
+    <br>
+</form>
+</body>
+</html>
